e16bf805fdb26e3ddb939ab2ee79007bbf4b0b9d
Vincenzo Petrolo <vincenzo.petrolo99@gmail.com>
2023-12-10 00:16:09 +0100

    added callback

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python
 
 import terminal_interaction as ti
+
+import time
+
 import speech_recognition as sr
-import os
 
-def recognize_speech():
-    recognizer = sr.Recognizer()
-
-    with sr.Microphone() as source:
-        print("Say something:")
-        audio = recognizer.listen(source)
-
+def callback(recognizer : sr.Recognizer, audio):
+    # received audio data, now we'll recognize it using Google Speech Recognition
     try:
-        text = recognizer.recognize_google(audio)
-        return text
+        transcript = recognizer.recognize_google(audio)
+        print(transcript)
+        if (transcript == "clear"):
+            ti.send_to_terminal("\x03")
+        else:
+            ti.send_to_terminal(f'shell-genie ask "{transcript}"')
     except sr.UnknownValueError:
-        return None
+        print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
-        return None 
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-if __name__ == "__main__":
-    while True:
-        transcription = recognize_speech()
 
-        if (transcription is not None):
-            if (transcription == "clear"):
-                os.system("^C") 
-            else:
-                # Generate zsh command
-                zsh_command = f'shell-genie ask "{transcription}"'
-                ti.send_to_terminal(zsh_command)
+r = sr.Recognizer()
+m = sr.Microphone()
+with m as source:
+    r.adjust_for_ambient_noise(source)  # we only need to calibrate once, before we start listening
+
+# start listening in the background (note that we don't have to do this inside a `with` statement)
+stop_listening = r.listen_in_background(m, callback)
+# `stop_listening` is now a function that, when called, stops background listening
+
+# do some unrelated computations for 5 seconds
+while(1):
+    print("LoquEnz is running")
+    time.sleep(20)  # we're still listening even though the main thread is doing other things
